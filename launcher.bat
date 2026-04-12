87f0420c04a3fb33f89bd06d3719bcc75ce58cd6
14ag <muriukipn@gmail.com>
2026-04-12 18:05:26 +0300

    backend init

--- launcher.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-pushd backend
-python -m venv .venv
-call .venv\Scripts\activate
-uv pip install -r requirements.txt
-popd
-settimeout 1 && cmd /c "python -m fastapi dev backend\main.py"
-start "" "http://localhost:8000"
-pause
