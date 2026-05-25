72f74de8e1b227cbd06cf750e0482183a5685926
14ag <muriukipn@gmail.com>
2026-05-25 06:20:44 +0300

    cleanup: remove unnecessary files and update data.json

--- test_responses.bat
+++ test_responses.bat
@@ -1,5 +1,28 @@
 @echo off
+goto :main
+
+
+:put
 curl -g -X PUT "http://127.0.0.1:8000/projects" ^
     -H "Content-Type: application/json" ^
     -d "{\"project-repo\":{\"title\":\"test title\",\"description\":\"long description man\",\"techs\":[\"java\", \"screen\"],\"_url\":\"https://github.com/14ag/blinter-vscode-extension\",\"category\":\"extensions\"}}"
+exit /b
+
+
+:get
+curl -g -X GET "http://127.0.0.1:8000/projects"
+exit /b
+
+
+:get-cors
+curl -I -X OPTIONS http://localhost:8000/projects ^
+  -H "Origin: http://localhost:3000" ^
+  -H "Access-Control-Request-Method: GET"
+exit /b
+
+
+:main
+call :get-cors
+echo done
 pause >nul
+
